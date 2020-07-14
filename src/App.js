e932b70d7753e873daca0c9b3b44ad42362c5f87
daisyna <sdaisyna335@gmail.com>
2020-07-14 12:38:07 +0545

    Som fix

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ import AllProduct from './components/view-pages/AllProducts';
 import Accessories from './components/view-pages/Accessories';
 import Clothes from './components/view-pages/Clothes';
 import Shoes from './components/view-pages/Shoes';
+import ViewProductDetails from './components/view-pages/ViewProductDetails';
 
 function App() {
   return (
@@ -48,6 +49,7 @@ function App() {
         <Route exact path='/kidsProducts' component={KidsProduct} />
 
         <Route exact path='/allProducts' component={AllProduct} />
+        <Route exact path='/viewproductdetails/:id' component={ViewProductDetails} />
 
         <Route exact path='/accessories' component={Accessories} />
         <Route exact path='/clothes' component={Clothes} />
